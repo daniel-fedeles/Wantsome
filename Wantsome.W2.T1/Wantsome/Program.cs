746e28a3f262c38fb1d1e2ebbbde410f2c52bbe4
DanielFe <DanielFe@DANIELFE-LAP>
2019-10-18 15:29:40 +0300

    W2.T1

--- Wantsome.W2.T1/Wantsome/Program.cs
+++ Wantsome.W2.T1/Wantsome/Program.cs
@@ -6,36 +6,62 @@ namespace Wantsome
     {
         static void Main(string[] args)
         {
-            //            EvenNumbers(15, 97);
-            //            Console.WriteLine("-------------------");
-            //            divbyt(20, 65);
-            //            Console.WriteLine("-------------------");
-            //            CountNr(1400, 2300);
-            //            Console.WriteLine("-------------------");
+            EvenNumbers(15, 97);
+            Console.WriteLine("-------------------");
+            divbyt(20, 65);
+            Console.WriteLine("-------------------");
+            CountNr(1400, 2300);
+            Console.WriteLine("-------------------");
             //            GuessANumber();
-            //            Console.WriteLine("-------------------");
-            //            PrintInReverse();
-            //            Console.WriteLine("-------------------");
-            //            PrintNumbersWithout(1, 10, 4, 7);
-            //            Console.WriteLine("-------------------");
-            //            Fibonacci(50);
-            //            Console.WriteLine("-------------------");
-            //            FizzBuzz(1, 1000);
-            //            Console.WriteLine("-------------------");
+            Console.WriteLine("-------------------");
+            PrintInReverse();
+            Console.WriteLine("-------------------");
+            PrintNumbersWithout(1, 10, 4, 7);
+            Console.WriteLine("-------------------");
+            Fibonacci(50);
+            Console.WriteLine("-------------------");
+            FizzBuzz(1, 1000);
+            Console.WriteLine("-------------------");
             int[] i = new int[] { 1, 3, 5, 8, 9, 1, 3, 6, 5, 9, 2, 5, 4, 7, 5, 6, 100, 2, 1, 3, 6, 9, 8, 5, 2, 1, 5 };
             int[] j = new int[] { 2, 5, 4, 7, 5, 6, 2, 1, 3, 6, 9, 8, 5, 2, 1, 5, 20, 9, 100 };
-            //FreequencyOfEachElement(i);
-            //            Console.WriteLine("-------------------");
-            //            SeparateOddAndEven(i);
-            //            Console.WriteLine("-------------------");
-            //            CommonElementsBetweenArrays(i, j);
+            FreequencyOfEachElement(i);
             Console.WriteLine("-------------------");
-            //            StringWithUniqueCharacters("abcdefabcf");
+            SeparateOddAndEven();
+            Console.WriteLine("-------------------");
+            CommonElementsBetweenArrays(i, j);
+            Console.WriteLine("-------------------");
+            StringWithUniqueCharacters("abcdefabcf");
             Console.WriteLine("-------------------");
             SumOfArray(i);
-
+            Console.WriteLine("-------------------");
+            RemoveDuplicatesFromLinkedList();
+            Console.WriteLine("-------------------");
+            PrimeOrNot();
+            Console.WriteLine("-------------------");
+            Palindrome();
+            Console.WriteLine("-------------------");
+            SumOfArray(i);
+            Console.WriteLine("-------------------");
+            AverageValueArray(i);
+            Console.WriteLine("-------------------");
+            FindIndexOfAnArray(i, 4);
+            Console.WriteLine("-------------------");
+            FindIndexOfAnArrayFromValue(i, 3);
+            Console.WriteLine("-------------------");
+            RemoveElement(i, 100, 0);
+            Console.WriteLine("-------------------");
+            InsertElementToSpecificPosition(i, 6, 25);
+            Console.WriteLine("-------------------");
+            MaximAndMinOfAnArray();
+            Console.WriteLine("-------------------");
+            CoppyElementFromOneArrayToAnother(i);
 
         }
+
+        /// <summary>Print to console all even numbers between 15 and 97.</summary>
+        /// <param name="nr1">The NR1.</param>
+        /// <param name="nr2">The NR2.</param>
+        /// 
         static void EvenNumbers(int nr1, int nr2)
         {
             for (int i = nr1; i <= nr2; i++)
@@ -47,6 +73,9 @@ namespace Wantsome
             }
         }
 
+        /// <summary>Print all numbers divisible by 3 between 20 and 65.</summary>
+        /// <param name="nr1">The NR1.</param>
+        /// <param name="nr2">The NR2.</param>
         static void divbyt(int nr1, int nr2)
         {
             for (int i = nr1; i <= nr2; i++)
@@ -58,6 +87,9 @@ namespace Wantsome
             }
         }
 
+        /// <summary>Count all numbers divisible by 7 and multiple of 5, from 1400 to 2300 and print the result to console</summary>
+        /// <param name="nr1">The NR1.</param>
+        /// <param name="nr2">The NR2.</param>
         static void CountNr(int nr1, int nr2)
         {
             var count = 0;
@@ -66,12 +98,13 @@ namespace Wantsome
                 if (i % 7 == 0 || i % 5 == 0)
                 {
                     count++;
-                    //                    Console.WriteLine(i);
                 }
             }
             Console.WriteLine(count);
         }
 
+        /// <summary>Write a program to guess a number between 1 and 10. To generate a random
+        /// number, use Random class from .NET Framework.</summary>
         static void GuessANumber()
         {
             int yourNumber = 0;
@@ -98,6 +131,9 @@ namespace Wantsome
             }
         }
 
+        /// <summary>
+        /// Read a text from console and print it reversed.
+        /// </summary>
         static void PrintInReverse()
         {
             Console.WriteLine("Enter your text");
@@ -108,6 +144,13 @@ namespace Wantsome
             }
         }
 
+        /// <summary>
+        /// Print numbers from 1 to 10 except 4 and 7.
+        /// </summary>
+        /// <param name="nr1">The NR1.</param>
+        /// <param name="nr2">The NR2.</param>
+        /// <param name="nr3">The NR3.</param>
+        /// <param name="nr4">The NR4.</param>
         static void PrintNumbersWithout(int nr1, int nr2, int nr3, int nr4)
         {
             for (int i = nr1; i < nr2; i++)
@@ -120,6 +163,11 @@ namespace Wantsome
             }
         }
 
+        /// <summary>
+        /// Print the Fibonacci sequence from 0 to 50. (i.e. Exery next number is found by
+        /// adding up those two before it: 0, 1, 1, 2, 3, 5, 8, 13, ...)
+        /// </summary>
+        /// <param name="nr2">The NR2.</param>
         static void Fibonacci(int nr2)
         {
             int x = 0;
@@ -149,6 +197,16 @@ namespace Wantsome
 
         }
 
+        /// <summary>
+        /// FizzBuzz
+        /// For all numbers from 1 to 1000, print the following:
+        /// - If number is divisible by 3, print Fizz
+        /// - If number is divisible by 5, print Buzz
+        /// - If number is divisible by 3 and 5, print FizzBuzz
+        /// - Otherwise, print the number
+        /// </summary>
+        /// <param name="nr1">The NR1.</param>
+        /// <param name="nr2">The NR2.</param>
         static void FizzBuzz(int nr1, int nr2)
         {
 
@@ -176,6 +234,10 @@ namespace Wantsome
 
         }
 
+        /// <summary>
+        /// Write a program to count the frequency of each element in an array
+        /// </summary>
+        /// <param name="arr">The arr.</param>
         static void FreequencyOfEachElement(int[] arr)
         {
             int[] freeq = new int[arr.Length];
@@ -211,8 +273,29 @@ namespace Wantsome
 
         }
 
-        static void SeparateOddAndEven(int[] arr)
+        /// <summary>
+        /// Read an array from console and separate odd and even values into 2 arrays.
+        /// </summary>
+        /// <param name="arr">The arr.</param>
+        static void SeparateOddAndEven()
         {
+            int[] arr = new int[9];
+            Console.WriteLine("Add integers to the array");
+            int index = 0;
+            while (index <= arr.Length - 1)
+            {
+                Console.WriteLine("Press enter after each integer");
+
+                arr[index] = Convert.ToInt32(Console.ReadLine());
+                if (index == 9)
+                {
+                    break;
+                }
+
+                index++;
+                Console.WriteLine(index);
+            }
+
             int countOdd = 0;
             int countEven = 0;
 
@@ -251,6 +334,11 @@ namespace Wantsome
             PrintArrays(odd);
         }
 
+        /// <summary>
+        /// Find common elements between two arrays of integers.
+        /// </summary>
+        /// <param name="arr1">The arr1.</param>
+        /// <param name="arr2">The arr2.</param>
         static void CommonElementsBetweenArrays(int[] arr1, int[] arr2)
         {
             int[] commonElements;
@@ -290,6 +378,10 @@ namespace Wantsome
 
         }
 
+        /// <summary>
+        /// Implement an algorithm to determine if a string has all unique characters.
+        /// </summary>
+        /// <param name="mystring">The mystring.</param>
         static void StringWithUniqueCharacters(string mystring)
         {
             bool flag = false;
@@ -314,10 +406,18 @@ namespace Wantsome
             }
         }
 
+        /// <summary>
+        /// Removes the duplicates from linked list.
+        /// </summary>
         static void RemoveDuplicatesFromLinkedList()
         {
 
         }
+
+
+        /// <summary>
+        /// Write a function to check if a number is prime or not.
+        /// </summary>
         static void PrimeOrNot()
         {
             int n, i, m = 0, flag = 0;
@@ -337,11 +437,19 @@ namespace Wantsome
                 Console.Write("Number is Prime.");
 
         }
+        /// <summary>
+        /// Check if a word is a palindrome 
+        /// </summary>
         static void Palindrome()
         {
 
         }
 
+        /// <summary>
+        /// Sums the of array.
+        /// </summary>
+        /// <param name="arr">The arr.</param>
+        /// <returns></returns>
         static int SumOfArray(int[] arr)
         {
             int sum = 0;
@@ -353,11 +461,20 @@ namespace Wantsome
             return sum;
         }
 
+        /// <summary>
+        /// Averages the value array.
+        /// </summary>
+        /// <param name="arr">The arr.</param>
         static void AverageValueArray(int[] arr)
         {
             Console.WriteLine("Average = " + SumOfArray(arr) / arr.Length);
         }
 
+        /// <summary>
+        /// Finds the index of an array.
+        /// </summary>
+        /// <param name="arr">The arr.</param>
+        /// <param name="index">The index.</param>
         static void FindIndexOfAnArray(int[] arr, int index)
         {
             for (int i = 0; i < arr.Length; i++)
@@ -370,25 +487,83 @@ namespace Wantsome
             }
         }
 
-        static void RemoveElement()
+        static void FindIndexOfAnArrayFromValue(int[] arr, int value)
         {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == value)
+                {
+                    Console.WriteLine("For specific value: " + value + " the correspondent index is: " + i);
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes the element.
+        /// </summary>
+        static void RemoveElement(int[] arr, int value, int index)
+        {
+            int el = 0;
+            if (value != 0)
+            {
+                el = value;
+            }
+            else if (index != 0)
+            {
+                el = index;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i == el || arr[i] == el)
+                {
+                    Console.WriteLine("element to be removed " + i + " " + arr[i]);
+                    arr[i] = 0;
+                    break;
+                }
+            }
+
 
         }
 
-        static void InsertElementToSpecificPosition()
+        /// <summary>
+        /// Inserts the element to specific position.
+        /// </summary>
+        static void InsertElementToSpecificPosition(int[] arr, int index, int value)
         {
-
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == index)
+                {
+                    arr[i] = value;
+                    Console.WriteLine("Value " + value + " inserted at index " + index);
+                }
+            }
         }
 
+        /// <summary>
+        /// Maxims the and minimum of an array.
+        /// </summary>
         static void MaximAndMinOfAnArray()
         {
 
         }
 
-        static void CoppyElementFromOneArrayToAnother()
+        /// <summary>
+        /// Coppies the element from one array to another.
+        /// </summary>
+        static void CoppyElementFromOneArrayToAnother(int[] arr)
         {
-
+            int[] newArr = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                newArr[i] = arr[i];
+            }
         }
+        /// <summary>
+        /// Prints the arrays.
+        /// </summary>
+        /// <param name="arr">The arr.</param>
         static void PrintArrays(int[] arr)
         {
             for (int i = 0; i < arr.Length; i++)
