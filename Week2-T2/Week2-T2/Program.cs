41a2c7bf873543a88482b4361897eefaf9279307
DanielFe <DanielFe@DANIELFE-LAP>
2019-10-20 22:24:19 +0300

    Palindrome LengthLastString

---
+++ Week2-T2/Week2-T2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Week2_T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Palindrome("##$$##");
+            //            RemoveDuplicatesFromLinkedList();
+            LengthLastString(" gigi Kent");
+        }
+
+        static void Palindrome(string word)
+        {
+            var x = String.Compare(ReverseString(word), word);
+
+            if (x == 0)
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not palindrome");
+            }
+        }
+
+        static void LengthLastString(string words)
+        {
+
+            string reverse = ReverseString(words);
+            int countWords = 0;
+            int countLetters = 0;
+
+            for (int i = 0; i < reverse.Length; i++)
+            {
+                if (!(words[i] == ' ' && i == 0))
+                {
+                    if (words[i] == ' ')
+                    {
+                        countWords++;
+                    }
+                }
+
+            }
+
+            if (countWords > 0)
+            {
+                for (int i = 0; i < reverse.Length; i++)
+                {
+                    if (!(words[i] == ' ' && i == 0))
+                    {
+                        if (words[i] == ' ')
+                        {
+                            break;
+
+                        }
+                        countLetters++;
+                    }
+
+                }
+            }
+            Console.WriteLine(countLetters);
+
+
+
+        }
+
+        static string ReverseString(string word)
+        {
+            string reverseWord = String.Empty;
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reverseWord = reverseWord + word[i];
+            }
+
+            return reverseWord;
+        }
+
+    }
+
+
+
+}
